55835772ed365aa7eec47bb047b9ae1265fd714e
Mueez Khan <30333942+rzmk@users.noreply.github.com>
2024-06-21 01:32:09 -0400

    docs: fix czv-wasm node-demo

--- czv-wasm/examples/node-demo/sample.ts
+++ czv-wasm/examples/node-demo/sample.ts
@@ -1,3 +1,5 @@
+// See the README.md file in this file's folder
+// for how to run this example
 import czv from "../../pkg/czv-ts";
 
 const options: czv.RowCountOptions = {
@@ -5,6 +7,7 @@ const options: czv.RowCountOptions = {
 apple,2.50
 banana,3.00
 strawberry,1.50`,
+    include_header_row: true
 };
 
 const output: number = czv.rowCount(options);
